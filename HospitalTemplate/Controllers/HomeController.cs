22f2980a92709ac87e1eba81ed644d39153fffe8
albayrak <albayrak@DESKTOP-4SREEGE>
2023-03-09 12:45:44 +0300

    Proje dosyası ekle.

---
+++ HospitalTemplate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Anasayfa = "active";
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Hakkinda = "active";
+            return View();
+        }
+        public ActionResult Service()
+        {
+            ViewBag.Hizmetler = "active";
+            return View();
+        }
+        public ActionResult Pricing()
+        {
+            ViewBag.Fiyatlandirma = "active";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.İletisim = "active";
+            return View();
+        }
+    }
+}
